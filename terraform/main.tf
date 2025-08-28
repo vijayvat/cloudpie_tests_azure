1b8452862cfb086aae577e124fb9314c57529e43
vijayvat <vpgvijay@yahoo.com>
2025-08-29 02:41:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   use_cli = false
   use_msi = false
   use_oidc = false  # Explicitly disable OIDC
-  subscription_id = env("CLOUD_ACCOUNT_ID")
+  subscription_id = "97ceb8b2-62ab-4c5d-aa68-d0a23f2fd59b"
 }
 
 data "azurerm_resource_group" "test" {
